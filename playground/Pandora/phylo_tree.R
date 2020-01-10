cf702f1999c90e1fc5ac14bfe89d576342f76d96
Sarah Pohl <sarah.pohl@helmholtz-hzi.de>
2020-01-10 15:02:02 +0100

    corrected broken link

--- playground/Pandora/phylo_tree.R
+++ playground/Pandora/phylo_tree.R
@@ -17,6 +17,7 @@ core_mat_tree <- nj(core_mat)
 # visualise tree (isolate names are apparently a bit too long so the plot needs tweaking)
 tr <- ggtree(core_mat_tree, layout="rectangular")
 tr <- tr %<+% background + geom_tiplab(aes(color=factor(Background)), size=4)
+tr <- tr + scale_color_manual(values=c("black", "darkgray"))
 tr <- tr + coord_cartesian(clip = 'off') + 
   theme_tree(plot.margin=margin(6, 60, 6, 6))
 tr
